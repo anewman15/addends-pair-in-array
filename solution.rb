fefcbf093197552bb18602d03912091e6fda0a7c
Abdullah Numan <anewman15@hotmail.com>
2021-06-17 18:11:29 +0600

    Fix indentation issues

--- solution.rb
+++ solution.rb
@@ -1,19 +1,19 @@
 # Brute Force O(n^2) Solution:
 
 def num_pair(arr, target)
-	result = []
-	for i in 0...arr.length
+  result = []
+  for i in 0...arr.length
     for j in 0...arr.length
-       if target - arr[i] == arr[j] && !result.include?(arr[j])
+      if target - arr[i] == arr[j] && !result.include?(arr[j])
         result << arr[i]
         result << target-arr[i]
-       end
-		j += 1
-	end
-	i += 1
-	end
-	result
+      end
+      j += 1
+    end
+    i += 1
+  end
+  result
 end
 
-nums = [2,7,11,15]
+nums = [2, 7, 11, 15]
 p num_pair(nums, 9)
